6f83c38ad27103fa8f0e22909d526776c7a2a52e
nishantjainjain3-glitch <nishantjain.jain3@gmail.com>
2026-06-26 14:49:54 +0530

    Deploy preview/app.js clean rebuild relative path fix

--- preview/app.js
+++ preview/app.js
@@ -1778,6 +1778,7 @@ function TeaCard({tea:e,onView:t,onImageClick:a,index:n=0}){
       )
     )
   )
+)
 }function ImageLightbox({ img: e, name: t, onClose: a }) {
   return (
     useEffect(() => {
